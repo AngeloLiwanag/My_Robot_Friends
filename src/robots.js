994972f003f6e6f9e0fd4b8f1c3f946a0c1d50c6
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2020-03-21 14:35:38 -0700

    filtering with react

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Angelo Liwanag",
+        username: "Peppy",
+        email: "Peppy@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Andrew Hua",
+        username: "Mj",
+        email: "Mj@gmail.com"
+    },
+    {
+        id: 3,
+        name: "James Wang",
+        username: "Chicken",
+        email: "Chicken@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Calvin Ho",
+        username: "Pende",
+        email: "Pende@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Henry Le",
+        username: "Goog",
+        email: "Goog@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Matt Ngo",
+        username: "Scoot",
+        email: "Scoot@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Ryan Doung",
+        username: "Micro",
+        email: "Micro@gmail.com"
+    },
+]
